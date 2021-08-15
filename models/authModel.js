3ee270f5ebe4a8fe27190c1632530c2862bd9619
Ashutosh Singh <ashu9993063472@gmail.com>
2021-08-15 17:24:09 +0530

    email validation added through validator

--- models/authModel.js
+++ models/authModel.js
@@ -49,12 +49,6 @@ const userSchema = mongoose.Schema(
   }
 );
 
-function validateEmail(email) {
-  const re =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-}
-
 userSchema.pre("save", async function (next) {
   // will run when user.save() or user.create({});
   if (!this.isModified("password")) {
